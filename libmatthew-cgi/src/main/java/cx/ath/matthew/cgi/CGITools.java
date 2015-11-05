00e07b57e6ce610ecb25f8341c8e5bd45b87ab3d
Bruno Oliveira <bruno@abstractj.org>
2015-11-05 00:24:29 -0200

    Update all the classes to version 0.8
    - Merge the modules libmatthew-hexdump and libmatthew-debug into libmatthew-unix. It makes more sense once this is the single place of usage
    - Starting to prepare pom file to release it on sonatype

--- libmatthew-cgi/src/main/java/cx/ath/matthew/cgi/CGITools.java
+++ libmatthew-cgi/src/main/java/cx/ath/matthew/cgi/CGITools.java
@@ -1,24 +1,29 @@
-/* 
- * CUAG AutoUmpire System
+/*
+ * Java CGI Library
+ *
+ * Copyright (c) Matthew Johnson 2004
  * 
- * Copyright (C) 2003/2004 Matthew Johnson, Adam Biltcliffe,
- * Michael Cripps, Martin O'Leary, Edward Allcutt and James Osborn
- *
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU Lesser General Public License 
- * as published by the Free Software Foundation, version 2 only.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details. 
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * To Contact the authors, please email assassins@srcf.ucam.org
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * To Contact the author, please email src@matthew.ath.cx
  *
  */
-
 
 package cx.ath.matthew.cgi;
 
